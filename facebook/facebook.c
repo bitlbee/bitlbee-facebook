bitlbee/bitlbee-facebook

commit 06956d1aae2252b33a8f981e3b73c4cd1c2452b6

facebook: create group chats directly with the fbjoin command
Currently, the plugin relies on the 'chat add' root command in order to
create a new group chat. Aside from being an ugly hack, this prevents
the plugin from being able to automatically join the bitlbee user into
the newly created channel. In order to allow for automatic joining, the
plugin should directly create the group chat, rather than relying on
the "chat add" root command.

facebook/facebook.c
@@ -399,6 +399,63 @@ void fb_data_free(fb_data_t *fata)
 }
 
 /**
+ * Creates a new #groupchat and adds it to the #fb_data. The returned
+ * #groupchat should be freed with #fb_data_groupchat_free() when no
+ * longer needed.
+ *
+ * @param ic   The #im_connection.
+ * @param tid  The thread #fb_id.
+ * @param name The name of the channel.
+ *
+ * @return The #groupchat or NULL on error.
+ **/
+struct groupchat *fb_data_groupchat_new(struct im_connection *ic,
+                                        fb_id_t tid,
+                                        const gchar *name)
+{
+    fb_data_t        *fata = ic->proto_data;
+    struct groupchat *gc;
+    gchar             stid[FB_ID_STRMAX];
+
+    FB_ID_TO_STR(tid, stid);
+
+    if (bee_chat_by_title(ic->bee, ic, stid) != NULL)
+        return NULL;
+
+    gc = imcb_chat_new(ic, stid);
+    fata->gcs = g_slist_prepend(fata->gcs, gc);
+
+    if (name != NULL)
+        imcb_chat_name_hint(gc, name);
+
+    imcb_chat_add_buddy(gc, ic->acc->user);
+    fb_api_thread_info(fata->api, tid);
+
+    return gc;
+}
+
+/**
+ * Frees all memory used by a #groupchat and removes it from the
+ * #fb_data.
+ *
+ * @param gc The #groupchat.
+ **/
+void fb_data_groupchat_free(struct groupchat *gc)
+{
+    fb_data_t *fata;
+
+    if (G_UNLIKELY(gc == NULL))
+        return;
+
+    if (G_LIKELY(gc->ic != NULL)) {
+        fata = gc->ic->proto_data;
+        fata->gcs = g_slist_remove(fata->gcs, gc);
+    }
+
+    imcb_chat_free(gc);
+}
+
+/**
  * Implements #prpl->init(). This initializes an account.
  *
  * @param acc The #account.
@@ -608,10 +665,7 @@ void fb_chat_invite(struct groupchat *gc, char *who, char *message)
  **/
 void fb_chat_leave(struct groupchat *gc)
 {
-    fb_data_t *fata = gc->ic->proto_data;
-
-    fata->gcs = g_slist_remove(fata->gcs, gc);
-    imcb_chat_free(gc);
+    fb_data_groupchat_free(gc);
 }
 
 /**
@@ -647,13 +701,15 @@ struct groupchat *fb_chat_join(struct im_connection *ic, const char *room,
     fb_id_t           tid;
     struct groupchat *gc;
 
-    gc = imcb_chat_new(ic, room);
-    fata->gcs = g_slist_prepend(fata->gcs, gc);
-    imcb_chat_add_buddy(gc, ic->acc->user);
-
     tid = FB_ID_FROM_STR(room);
-    fb_api_thread_info(fata->api, tid);
+    gc  = fb_data_groupchat_new(ic, tid, NULL);
 
+    if (gc == NULL) {
+        imcb_error(fata->ic, "Failed to join chat: %" FB_ID_FORMAT, tid);
+        return NULL;
+    }
+
+    fb_api_thread_info(fata->api, tid);
     return gc;
 }
 
@@ -870,9 +926,9 @@ static void fb_cmd_fbjoin(irc_t *irc, char **args)
     account_t *acc;
     fb_data_t *fata;
     fb_id_t   *tid;
+    gchar     *name;
     guint      oset;
     gint64     indx;
-    gchar      stid[FB_ID_STRMAX];
 
     acc = fb_cmd_account(irc, args, 2, &oset);
 
@@ -880,6 +936,7 @@ static void fb_cmd_fbjoin(irc_t *irc, char **args)
         return;
 
     fata = acc->ic->proto_data;
+    name = args[oset + 1];
     indx = g_ascii_strtoll(args[oset], NULL, 10);
     tid  = g_slist_nth_data(fata->tids, indx - 1);
 
@@ -888,10 +945,10 @@ static void fb_cmd_fbjoin(irc_t *irc, char **args)
         return;
     }
 
-    FB_ID_TO_STR(*tid, stid);
-
-    gchar *cmd[] = {"chat", "add", acc->tag, stid, args[oset + 1], NULL};
-    root_command(irc, cmd);
+    if (fb_data_groupchat_new(acc->ic, *tid, name) == NULL) {
+        imcb_error(fata->ic, "Failed to join chat: %s (%" FB_ID_FORMAT ")",
+                   name, *tid);
+    }
 }
 
 /**

README
@@ -43,14 +43,12 @@ Usage:
   Group Chats (existing chat):
     > fbchats [acc]
     > fbjoin [acc] <index> <channel>
-    > /join #<channel>
     > /topic <message>
     > /invite <user>
 
   Group Chats (creating chat):
     > fbcreate [acc] <user,user,...>
     > fbjoin [acc] 1 <channel>
-    > /join #<channel>
     > /topic <message>
     > /invite <user>
 

facebook/facebook.h
@@ -45,4 +45,10 @@ fb_data_t *fb_data_new(account_t *acc);
 
 void fb_data_free(fb_data_t *fata);
 
+struct groupchat *fb_data_groupchat_new(struct im_connection *ic,
+                                        fb_id_t tid,
+                                        const gchar *name);
+
+void fb_data_groupchat_free(struct groupchat *gc);
+
 #endif /* _FACEBOOK_H */

commit 97f5a33c90cd22c4a03d3dda61e09be9b67091d9

facebook: default to chat_type of room for persistence
As of now, group chat channels do not persist after they are left. This
is due in part to the channel not being of chat_type room. Furthermore,
the account and room identifiers are not set on the channel.

This issue becomes apparent when the plugin disconnects from the remote
services for whatever reason, and the channel is not setup again.

This issue was introduced in 06956d1.

facebook/facebook.c
@@ -415,6 +415,7 @@ struct groupchat *fb_data_groupchat_new(struct im_connection *ic,
 {
     fb_data_t        *fata = ic->proto_data;
     struct groupchat *gc;
+    irc_channel_t    *ch;
     gchar             stid[FB_ID_STRMAX];
 
     FB_ID_TO_STR(tid, stid);
@@ -422,15 +423,32 @@ struct groupchat *fb_data_groupchat_new(struct im_connection *ic,
     if (bee_chat_by_title(ic->bee, ic, stid) != NULL)
         return NULL;
 
-    gc = imcb_chat_new(ic, stid);
-    fata->gcs = g_slist_prepend(fata->gcs, gc);
-
     if (name != NULL) {
         if (strchr(CTYPES, name[0]) != NULL)
             name++;
+
+        /* Let the hackery being... */
+        gc = imcb_chat_new(ic, stid);
         imcb_chat_name_hint(gc, name);
+
+        ch = gc->ui_data;
+        ch->flags |= IRC_CHANNEL_CHAT_PICKME;
+
+        /* Setup the channel as a room */
+        set_setstr(&ch->set, "type",      "chat");
+        set_setstr(&ch->set, "chat_type", "room");
+        set_setstr(&ch->set, "account",   ic->acc->tag);
+        set_setstr(&ch->set, "room",      stid);
+
+        /* Free and recreate with new channel settings */
+        imcb_chat_free(gc);
     }
 
+    gc = imcb_chat_new(ic, stid);
+    ch = gc->ui_data;
+    fata->gcs = g_slist_prepend(fata->gcs, gc);
+
+    ch->flags &= ~IRC_CHANNEL_CHAT_PICKME;
     imcb_chat_add_buddy(gc, ic->acc->user);
     fb_api_thread_info(fata->api, tid);
 
@@ -712,7 +730,6 @@ struct groupchat *fb_chat_join(struct im_connection *ic, const char *room,
         return NULL;
     }
 
-    fb_api_thread_info(fata->api, tid);
     return gc;
 }
 