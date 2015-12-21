bitlbee/bitlbee-facebook

commit 0121bae03b6d545ff799f4b02c9b05becac1b78b

facebook-json: Ensure data is null terminated for json-glib < 1.0.2
Older json-glib versions had a bug[1] in which the length parameter was
ignored and this error happened if the input was not null-terminated:

    JSON data must be UTF-8 encoded

Since these versions are expected to still be around in some distros,
this commit makes a copy with g_strndup() to ensure that it's always
null terminated.

Thanks to advcomp2019 for reporting this bug and finding a test case
where this issue is reproducible every time (receiving events of people
joining or leaving in a groupchat)

[1]: https://bugzilla.gnome.org/show_bug.cgi?id=727755

facebook/facebook-json.c
@@ -252,13 +252,18 @@ fb_json_bldr_add_strf(JsonBuilder *bldr, const gchar *name,
 JsonNode *
 fb_json_node_new(const gchar *data, gssize size, GError **error)
 {
+    gchar *slice;
     JsonNode *root;
     JsonParser *prsr;
 
+    /* Ensure data is null terminated for json-glib < 1.0.2 */
+    slice = g_strndup(data, size);
+
     prsr = json_parser_new();
 
-    if (!json_parser_load_from_data(prsr, data, size, error)) {
+    if (!json_parser_load_from_data(prsr, slice, size, error)) {
         g_object_unref(prsr);
+        g_free(slice);
         return NULL;
     }
 
@@ -266,6 +271,7 @@ fb_json_node_new(const gchar *data, gssize size, GError **error)
     root = json_node_copy(root);
 
     g_object_unref(prsr);
+    g_free(slice);
     return root;
 }
 

commit 28ec26e7ca9b1909c65939cea7f55fc72c55ed9c

facebook-json: fixed a size overflow with string duplication
Unlike json_parser_load_from_data(), g_strndup() will not handle signed
sizes that are negative. This causes the size to overflow to a really
large value, and in turn lead to a segmentation fault.

The solution is simple: calculate the size of the data when the given
size is negative.

This bug was introduced by 0121bae.

facebook/facebook-json.c
@@ -256,9 +256,14 @@ fb_json_node_new(const gchar *data, gssize size, GError **error)
     JsonNode *root;
     JsonParser *prsr;
 
+    g_return_val_if_fail(data != NULL, NULL);
+
+    if (size < 0) {
+        size = strlen(data);
+    }
+
     /* Ensure data is null terminated for json-glib < 1.0.2 */
     slice = g_strndup(data, size);
-
     prsr = json_parser_new();
 
     if (!json_parser_load_from_data(prsr, slice, size, error)) {