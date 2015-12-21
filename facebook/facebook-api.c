00c0ae832b2f04969d205b951ae37b9bc884b84f
jgeboski <jgeboski@gmail.com>
2015-12-20 19:30:25 -0500

    facebook-api: fixed non-friend contacts showing as buddies

--- facebook/facebook-api.c
+++ facebook/facebook-api.c
@@ -1896,6 +1896,8 @@ fb_api_cb_contacts(FbHttpRequest *req, gpointer data)
     fb_json_values_add(values, FB_JSON_TYPE_STR, TRUE,
                        "$.graph_api_write_id");
     fb_json_values_add(values, FB_JSON_TYPE_STR, TRUE,
+                       "$.represented_profile.friendship_status");
+    fb_json_values_add(values, FB_JSON_TYPE_STR, TRUE,
                        "$.represented_profile.id");
     fb_json_values_add(values, FB_JSON_TYPE_STR, TRUE,
                        "$.structured_name.text");
@@ -1907,6 +1909,12 @@ fb_api_cb_contacts(FbHttpRequest *req, gpointer data)
     while (fb_json_values_update(values, &err)) {
         g_free(writeid);
         writeid = fb_json_values_next_str_dup(values, NULL);
+        str = fb_json_values_next_str(values, NULL);
+
+        if (g_strcmp0(str, "ARE_FRIENDS") == 0) {
+            continue;
+        }
+
         user = fb_api_user_dup(NULL, FALSE);
         str = fb_json_values_next_str(values, "0");
 
