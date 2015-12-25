bitlbee/bitlbee-facebook

commit 00c0ae832b2f04969d205b951ae37b9bc884b84f

facebook-api: fixed non-friend contacts showing as buddies

facebook/facebook-api.c
@@ -1896,6 +1896,8 @@ fb_api_cb_contacts(FbHttpRequest *req, gpointer data)
     fb_json_values_add(values, FB_JSON_TYPE_STR, TRUE,
                        "$.graph_api_write_id");
     fb_json_values_add(values, FB_JSON_TYPE_STR, TRUE,
+                       "$.represented_profile.friendship_status");
+    fb_json_values_add(values, FB_JSON_TYPE_STR, TRUE,
                        "$.represented_profile.id");
     fb_json_values_add(values, FB_JSON_TYPE_STR, TRUE,
                        "$.structured_name.text");
@@ -1907,6 +1909,12 @@ fb_api_cb_contacts(FbHttpRequest *req, gpointer data)
     while (fb_json_values_update(values, &err)) {
         g_free(writeid);
         writeid = fb_json_values_next_str_dup(values, NULL);
+        str = fb_json_values_next_str(values, NULL);
+
+        if (g_strcmp0(str, "ARE_FRIENDS") == 0) {
+            continue;
+        }
+
         user = fb_api_user_dup(NULL, FALSE);
         str = fb_json_values_next_str(values, "0");
 

commit c2a7c914d3d218124cb3cfd7ca422d2d7c37fa67

facebook: fixed broken contact counting with non-friends
When fetching more than 500 contacts, multiple requests must be made in
order avoid request limits. When friends are retrieved and processed,
the plugin must keep a count of the processed contacts to know whether
or not it needs to retrieve more contacts.

Currently, this counter assumes every contact is a friend, which is not
always the case. This counter needs to advance even when a contact is
not a friend.

This fixes the incomplete fetching of the buddy list when a user has
over 500 friends.

This is a regression introduced by 00c0ae8.

facebook/facebook-api.c
@@ -1910,6 +1910,7 @@ fb_api_cb_contacts(FbHttpRequest *req, gpointer data)
         g_free(writeid);
         writeid = fb_json_values_next_str_dup(values, NULL);
         str = fb_json_values_next_str(values, NULL);
+        count++;
 
         if (g_strcmp0(str, "ARE_FRIENDS") != 0) {
             continue;
@@ -1926,8 +1927,6 @@ fb_api_cb_contacts(FbHttpRequest *req, gpointer data)
         fb_http_values_parse(prms, user->icon, TRUE);
         user->csum = fb_http_values_dup_str(prms, "oh", &err);
         fb_http_values_free(prms);
-
-        count++;
         users = g_slist_prepend(users, user);
     }
 