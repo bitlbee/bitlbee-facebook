8fcb9875b84c7df505ec3651dad6113f78fa81a5
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-05-14 23:16:03 +0100

    use FB_ORCA_AGENT from purple-facebook

--- facebook/facebook-api.h
+++ facebook/facebook-api.h
@@ -117,7 +117,7 @@
  *
  */
 
-#define FB_ORCA_AGENT "[FBAN/Orca-Android;FBAV/537.0.0.31.101;FBBV/14477681]"
+#define FB_ORCA_AGENT "[FBAN/Orca-Android;FBAV/537.0.0.31.101;FBPN/com.facebook.orca;FBLC/en_US;FBBV/52182662]"
 
 /**
  * FB_API_AGENT:
